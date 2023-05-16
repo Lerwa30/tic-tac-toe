d5d702359ad90eb81aa507945b4eac1868a6ea8d
Leroy <leroypawlick@gmail.com>
2023-05-15 19:04:09 -0700

    added props to component, applied css file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React, {useState} from 'react';
-import './App.css';
-import Square from './Square';
+import React, { useState } from "react";
+import "./App.css";
+import Square from "./Square";
 
 function App() {
-  const [squares, setSquares] = useState(['', '', '', '', '', '', '', '', '']);
+  const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState(true);
 
   return (
     <div className="App">
-      <Square squares={squares} setSquares={setSquares} player={player} setPlayer={setPlayer} 
+      <div className="container">{squares.map((value, index) => {
+        return  <Square
+        squares={squares}
+        setSquares={setSquares}
+        player={player}
+        setPlayer={setPlayer}
+        squareValue={value}
+        index={index}
       />
+      })}</div>
     </div>
   );
 }
