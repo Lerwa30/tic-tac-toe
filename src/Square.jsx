c12c229a19c5f32625875ff3720b75507a60c460
Leroy <leroypawlick@gmail.com>
2023-05-15 19:32:26 -0700

    added function to check for X and O

--- src/Square.jsx
+++ src/Square.jsx
@@ -3,8 +3,24 @@ import React from 'react';
 const Square = (props) => {
     console.log(props.squares, props.player)
     return (
-        <div className='square'></div>
+        <div className='square' onClick={handleClick}>{props.squareValue === 'O' ? < img src="https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png" alt='' /> : props.squareValue}
+            </div>
     );
+};
+
+const handleClick = (props) => {
+    if(props.squareValue === false) {
+        if(props.player === true) {
+            props.squares.splice(props.index, 1, 'X');
+            props.setSquares(props.squares);
+            props.setPlayer(null);
+        } else {
+            props.squares.splice(props.index, 1, 'O');
+            props.setSquares(props.squares);
+            props.setPlayer(null);
+        }
+
+    }
 };
 
 export default Square;
